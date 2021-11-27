25009cef6323f0c730bd3fa200714e7e7a59f579
Momo <normantraumueller@gmx.de>
2021-11-27 19:21:23 +0100

    Add: Structure

---
+++ ValhallaV/ValhallaVBackend/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AltV.Net;
+
+namespace ValhallaVBackend
+{
+    public class Main : Resource
+    {
+        public override void OnStart()
+        {
+            Alt.Log("ValhallaV gestartet.");
+        }
+
+        public override void OnStop()
+        {
+            Alt.Log("ValhallaV gestoppt!");
+        }
+    }
+}
